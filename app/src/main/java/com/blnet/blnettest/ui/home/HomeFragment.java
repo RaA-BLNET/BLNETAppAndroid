c6b8da043e2d277a68a79925e97c3c5217f9be84
RaA-BLNET <a.rahunenthiran@bbz-sh.ch>
2020-01-28 13:00:59 +0100

    blnet klein geschrieben

--- app/src/main/java/com/blnet/blnettest/ui/home/HomeFragment.java
+++ app/src/main/java/com/blnet/blnettest/ui/home/HomeFragment.java
@@ -16,12 +16,12 @@ import com.blnet.blnettest.R;
 
 public class homeFragment extends Fragment {
 
-    private homeViewModel homeViewModel;
+    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
-                ViewModelProviders.of(this).get(homeViewModel.class);
+                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
