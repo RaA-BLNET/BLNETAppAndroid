fca9ba3d87c452865f91edd814305bdc059889b2
RaA-BLNET <a.rahunenthiran@bbz-sh.ch>
2020-02-06 08:59:53 +0100

    registrieren

---
+++ app/src/main/java/com/blnet/blnettest/ui/user.java
@@ -0,0 +1,9 @@
+package com.blnet.blnettest.ui;
+
+public class user {
+    String vorname, nachname, email, passwort;
+
+    public User (String vorname, String nachname, String email, String passwort) {
+
+    }
+}
