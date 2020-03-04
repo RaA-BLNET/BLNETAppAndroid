3464bef3b59e1b331ef76e5549cdbafdc17aa751
RaA-BLNET <a.rahunenthiran@bbz-sh.ch>
2020-03-04 11:47:38 +0100

    ämtli checkliste final

--- app/src/main/java/com/blnet/blnettest/ui/checkliste/checklisteFragment.java
+++ app/src/main/java/com/blnet/blnettest/ui/checkliste/checklisteFragment.java
@@ -1,19 +1,19 @@
 package com.blnet.blnettest.ui.checkliste;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+        import androidx.annotation.NonNull;
+        import androidx.annotation.Nullable;
+        import androidx.fragment.app.Fragment;
+        import androidx.lifecycle.Observer;
+        import androidx.lifecycle.ViewModelProviders;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
 
-import com.blnet.blnettest.R;
+        import com.blnet.blnettest.R;
 
 
 public class checklisteFragment extends Fragment {
